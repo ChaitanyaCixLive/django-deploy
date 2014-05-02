f93151c78ca32a8f797889ad2f9a7d75d7ada547
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2014-05-02 20:26:37 +0200

    Added default deployment tasks.

---
+++ django_deploy/deploy.py
@@ -0,0 +1,25 @@
+import django
+from django.conf import settings
+
+from django_deploy.models import deploy_tasks, Task
+
+
+default_tasks = []
+
+if django.VERSION[1] < 7:
+    default_tasks.append(Task('syncdb', interactive=False))
+
+if 'south' in settings.INSTALLED_APPS or django.VERSION[1] >= 7:
+    default_tasks.append(Task('migrate'))
+
+if 'staticfiles' in settings.INSTALLED_APPS and settings.STATIC_ROOT:
+    default_tasks.append(Task('collectstatic', interactive=False))
+
+if settings.USE_I18N:
+    default_tasks.extend([
+        Task('makemessages', all=True),
+        Task('compilemessages'),
+    ])
+
+task_list = getattr(settings, 'DEPLOY_COMMANDS', default_tasks)
+tasks = deploy_tasks(*task_list)
